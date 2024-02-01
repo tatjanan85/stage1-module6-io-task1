9e06ff216029430d428dffe6c89fed46d468889d
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2024-02-01 10:12:54 +0300

    feat: module6 task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = "", email = "";
+        int age = 0;
+        long phone = 0;
+
+        try (BufferedReader in = new BufferedReader(new java.io.FileReader(file))) {
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                String[] values = str.split(": ");
+
+                switch (values[0]) {
+                    case "Name":
+                        name = values[1];
+                        break;
+                    case "Age":
+                        age = Integer.parseInt(values[1]);
+                        break;
+                    case "Email":
+                        email = values[1];
+                        break;
+                    case "Phone":
+                        phone = Long.parseLong(values[1]);
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(name, age, email, phone);
     }
 }
